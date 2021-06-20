e1744c7404d78271dd0833577a46ecf531394b2f
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-06-21 09:59:45 +1000

    Implement the simplest compile job

--- CompileFunction/__init__.py
+++ CompileFunction/__init__.py
@@ -1,4 +1,7 @@
 import logging
+import json
+import io
+import contextlib
 
 import azure.functions as func
 import pyjion
@@ -7,14 +10,27 @@ import pyjion.dis
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    result = {}
     pyjion.enable()
     pyjion.enable_pgc()
     co = compile(req.get_body(), 'demo.py', 'exec')
-
+    exec(co)
+    result['compile_result1'] = pyjion.info(co)
+    exec(co)
+    result['compile_result2'] = pyjion.info(co)
     pyjion.disable()
 
+    dis_cil = io.StringIO()
+    with contextlib.redirect_stdout(dis_cil):
+        pyjion.dis.dis(co, True)
+    result['dis_cil'] = dis_cil.getvalue()
+
+    dis_x64 = io.StringIO()
+    with contextlib.redirect_stdout(dis_x64):
+        pyjion.dis.dis_native(co, True)
+    result['dis_x64'] = dis_x64.getvalue()
+
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            json.dumps(result),
             status_code=200
     )
