5f97b5abefb897cd8eeb208a0dd8a2ede1ed9025
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-06-21 13:02:44 +1000

    Most of the application prototype working

--- CompileFunction/__init__.py
+++ CompileFunction/__init__.py
@@ -15,9 +15,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     pyjion.enable_pgc()
     pyjion.enable_graphs()
     co = compile(req.get_body(), 'demo.py', 'exec')
-    exec(co)
+    exec(co) # run the code once
     result['compile_result1'] = pyjion.info(co)
-    exec(co)
+    exec(co) # run the code again with profile data
     result['compile_result2'] = pyjion.info(co)
     pyjion.disable()
     result['graph'] = pyjion.get_graph(co)
@@ -30,7 +30,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     with contextlib.redirect_stdout(dis_x64):
         pyjion.dis.dis_native(co, True)
     result['dis_x64'] = dis_x64.getvalue()
-
+    result['version'] = pyjion.__version__
     return func.HttpResponse(
             json.dumps(result),
             status_code=200
