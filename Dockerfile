be9f4ec48223100c6600988fffd3b3e2702722ac
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-01-10 12:30:37 +1100

    Move things to a custom container

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+ARG DOTNET_VERSION=6.0.100
+FROM python:3.10-slim-buster
+ARG DOTNET_VERSION
+RUN echo "Building Pyjion with .NET $DOTNET_VERSION"
+COPY . /api
+WORKDIR /api
+
+ENV TZ=Europe/London
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+RUN apt-get -y update && apt-get install -y software-properties-common build-essential && apt-get -y update && apt-get install -y wget unzip bzip2 && apt-get clean -y && rm -rf /var/lib/apt/lists/*
+RUN wget https://dotnetcli.azureedge.net/dotnet/Sdk/${DOTNET_VERSION}/dotnet-sdk-${DOTNET_VERSION}-linux-x64.tar.gz
+RUN mkdir -p dotnet && tar zxf dotnet-sdk-${DOTNET_VERSION}-linux-x64.tar.gz -C dotnet
+ENV DOTNET_ROOT=/api/dotnet
+
+COPY ./api/requirements.txt /code/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+COPY ./api /code/app
+CMD ["python", "-m", "gunicorn", "--conf", "api/gunicorn_conf.py", "--bind", "0.0.0.0:80", "api.main:app"]
---
+++ api/main.py
@@ -0,0 +1,82 @@
+import logging
+import json
+import io
+import contextlib
+import sys
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+
+
+import pyjion
+import pyjion.dis
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/compile', methods=['POST'])
+def main():
+    logging.info('Python HTTP trigger function processed a request.')
+    result = {}
+    BLOCKED_EVENTS = [
+        'import',
+        'imaplib.send',
+        'ftplib.connect',
+        'ftplib.sendcmd',
+        'open',
+        'os.chmod',
+        'os.chown',
+        'os.exec',
+        'os.fork',
+        'os.remove',
+        'os.rmdir',
+        'os.spawn',
+        'os.system',
+        'smtplib.connect',
+        'socket.connect',
+        'subprocess.Popen',
+        'webbrowser.open',
+        'urllib.Request',
+        'flask.debughelpers'
+    ]
+    def block_imports(event, args):
+        if event in BLOCKED_EVENTS:
+            raise ValueError(f"Code not allowed ({event} {args}).")
+    level = request.args.get('level')
+    if not level or int(level) not in [0, 1, 2]:
+        level = 1
+    debug  = bool(request.args.get('debug'))
+    pyjion.config(graph=True, level=level, debug=debug)
+    pyjion.enable()
+
+    try:
+        sys.addaudithook(block_imports)
+        co = compile(request.data, 'demo.py', 'exec')
+        exec(co, {}, {}) # run the code once
+        exec(co, {}, {}) # run the code again with profile data
+        BLOCKED_EVENTS = []
+    except Exception as ex:
+        return (
+            str(ex),
+            500
+        )
+
+    pyjion.disable()
+    result['graph'] = pyjion.graph(co)
+    dis_cil = io.StringIO()
+    with contextlib.redirect_stdout(dis_cil):
+        pyjion.dis.dis(co, True, True)
+    result['dis_cil'] = dis_cil.getvalue()
+    result['offsets'] = pyjion.offsets(co)
+    dis_x64 = io.StringIO()
+    with contextlib.redirect_stdout(dis_x64):
+        pyjion.dis.dis_native(co, True, False)
+    result['dis_x64'] = dis_x64.getvalue()
+    result['cfg'] = pyjion.dis.flow_graph(co)
+    result['version'] = pyjion.__version__
+    result['config'] = pyjion.config()
+    return json.dumps(result)
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host="0.0.0.0", debug=False, port=80)
